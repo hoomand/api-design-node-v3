7623cc1fc564f36a438f36ab7900a81fa8b2b9d9
Bijan 🎧📚🎮 Hoomand <hoomand@gmail.com>
2019-06-19 08:51:23 +1000

    add Items route

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,22 @@
 import { Router } from 'express'
 
+const controller = (req, res) => {
+  res.send({ message: 'hello' })
+}
+
 const router = Router()
+
+// /api/item
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+// /api/item/:id
+router
+  .route('/:id')
+  .put(controller)
+  .delete(controller)
+  .get(controller)
 
 export default router
